ef0dea5c7c1e02196dcb99449ae762069bd2d8ef
rania333 <raniaaramadan1999@gmail.com>
2022-10-23 01:04:02 +0200

    validation

--- src/middleware/validateQuery.ts
+++ src/middleware/validateQuery.ts
@@ -9,8 +9,10 @@ export const validateQuery = (req: Request, res: Response, nxt: () => void ) => 
         res.status(400).send('you should enter filename, width, height like this => http://localhost:3000/api?filename=..&width=..&height=.. \n and here is valid file name' + availableFiles)
     } else if (!availableFiles.includes(filename)) {
         res.status(404).send('the filename not valid, please choose one from   ' + availableFiles)
-    } else if (width<=0 || height<=0) {
-        res.status(400).send('please enter valid width and height, greater than 0')
+    } else if (isNaN(width) || width <= 0) {
+        res.status(400).send('please enter valid width and greater than 0 ')
+    } else if (isNaN(height) || height <= 0) {
+        res.status(400).send('please enter valid height and greater than 0 ')
     } else {
         nxt()
     }
